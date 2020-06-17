80306b2a000ce88d155a3105c2e076464c484f41
teekaytech <taofeek.olalere@feghas.com>
2020-06-17 11:41:06 +0100

    add destroy feature to tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tagging = Tagging.find(@tag[:id])
+    @tagging.destroy
+    @tag.destroy
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+    redirect_to tags_path
+  end
 end
